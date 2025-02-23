a095bc3308099f87699cfbeaa4f50c9f89d1e238
gustino7 <mtgpmn2013@gmail.com>
2025-02-23 14:20:08 +0000

    first commit

---
+++ implement.py
@@ -0,0 +1,45 @@
+import yara
+import os
+
+def apply_yara_rules_to_malware(malware_directory, rules_directory):
+    # Mengambil semua file YARA rules dari direktori
+    rules_files = [f for f in os.listdir(rules_directory) if f.endswith('.yar') or f.endswith('.yara')]
+    index_rules = 1
+
+    # Iterasi melalui setiap file YARA rules
+    for rules_file in rules_files:
+        rules_path = os.path.join(rules_directory, rules_file)
+        print(f'\nLoading rules from: {rules_path}')
+
+        # Print Index Rules
+        print(f'Rules ke-{index_rules}; nama-{rules_path}')
+        index_rules += 1
+        
+        # Muat aturan YARA dari file
+        rules = yara.compile(filepath=rules_path)
+
+        # Iterasi melalui semua file malware dalam direktori
+        for malware_file in os.listdir(malware_directory):
+            malware_path = os.path.join(malware_directory, malware_file)
+
+            # Hanya memeriksa file, abaikan direktori
+            if os.path.isfile(malware_path):
+                try:
+                    # Menerapkan aturan YARA pada file malware
+                    matches = rules.match(malware_path)
+
+                    # Jika ada kecocokan, tampilkan hasil
+                    if matches:
+                        print(f'File: {malware_file} - Matches found in {rules_file}: {matches}')
+                    #else:
+                    #    print(f'File: {malware_file} - No matches found in {rules_file}.')
+                except Exception as e:
+                    print(f'Error processing {malware_file} with rules from {rules_file}: {e}')
+
+if __name__ == '__main__':
+    # Tentukan direktori untuk malware dan direktori untuk YARA rules
+    malware_directory = './malware/AgentTesla_MALW'     # Ganti dengan direktori malware Anda
+    rules_directory = './yara_rules/agentTesla'    	# Ganti dengan direktori yang berisi YARA rules Anda
+
+    # Panggil fungsi untuk menerapkan YARA rules ke malware
+    apply_yara_rules_to_malware(malware_directory, rules_directory)
